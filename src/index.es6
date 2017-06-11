8909d27724be6ab0c784a4892447e1f5bbcd684c
David Anderson <david@anderson.scot>
2017-06-11 16:17:46 +0100

    Implemented unit tests for all functionality. Implemented Roman Numeral to Integer conversion.

--- src/index.es6
+++ src/index.es6
@@ -1,12 +1,106 @@
 'use strict';
 
+const romanToArabicMap = {
+    'IV': 4,
+    'IX': 9,
+    'XL': 40,
+    'XC': 90,
+    'CD': 400,
+    'CM': 900,
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+};
+
+const arabicToRomanMap = [
+    1000,
+    900,
+    500,
+    400,
+    100,
+    90,
+    50,
+    40,
+    10,
+    9,
+    5,
+    4,
+    1
+];
+
+class RomanNumeralTranslator {
+    static _getNextToken (romanNumeral) {
+        if (romanNumeral === '') {
+            return '';
+        }
+
+        if (romanNumeral.length === 1) {
+            return romanNumeral;
+        }
+
+        let substractiveToken = romanNumeral.substring(0, 2);
+        let standardToken = romanNumeral.substring(0, 1);
+
+        if (romanToArabicMap.hasOwnProperty(substractiveToken)) {
+            return substractiveToken;
+        }
+
+        if (romanToArabicMap.hasOwnProperty(standardToken)) {
+            return standardToken;
+        }
+
+        throw Error(`The Roman Numeral ${standardToken} is not valid`);
+    }
+
+    static romanNumeralToInteger (romanNumeral) {
+
+        if (romanNumeral === '') {
+            return 0;
+        }
+
+        let nextToken = RomanNumeralTranslator._getNextToken(romanNumeral);
+        let sum = romanToArabicMap[nextToken];
+        let remainingString = romanNumeral.substring(nextToken.length);
+
+        sum += RomanNumeralTranslator.romanNumeralToInteger(remainingString);
+        return sum;
+    }
+
+    static integerToRomanNumeral (integer) {
+
+    }
+}
+
 class RomanNumeral {
+    static parseInteger (integer) {
+        throw 'Not Implemented';
+    }
+
     constructor (romanNumeral) {
-        this.value = romanNumeral;
+        this.romanValue = romanNumeral;
+
+        var arabicValue = RomanNumeralTranslator.romanNumeralToInteger(romanNumeral);
+        this._ensureIntegerIsValid(arabicValue);
+
+        this.arabicValue = arabicValue;
+    }
+
+    _ensureIntegerIsValid (integer) {
+        if (integer < 1 || integer > 3999) {
+            throw new RangeError(`Value ${integer} must be between I (1) and MMMCMXCIX (3999)`);
+        }
     }
 
     toString () {
-        return this.value;
+        return this.romanValue;
+    }
+
+    toInteger () {
+        throw 'Not Implemented';
     }
 }
 
--- test/romanNumeral.spec.js
+++ test/romanNumeral.spec.js
@@ -2,12 +2,73 @@
 
 const RomanNumeral = require('../lib/index.js');
 const chai = require('chai');
-chai.should();
+const should = chai.should();
 
 describe('the RomanNumeral class', () => {
     describe('when constructed with a roman numeral', () => {
+        it('should raise an exception when greater than 3999', () => {
+            should.throw(
+                () => new RomanNumeral('MMMM'),
+                RangeError,
+                'Value 4000 must be between I (1) and MMMCMXCIX (3999)');
+        });
+
+        it('should raise an exception when an invalid character is included', () => {
+            should.throw(
+                () => new RomanNumeral('MMSIX'),
+                Error,
+                'The Roman Numeral S is not valid'
+            );
+        });
+
+        it('should accept 1', () => {
+            var sut = new RomanNumeral('I');
+            sut.toString().should.equal('I');
+        });
+
+        it ('should accept 3999', () => {
+            var sut = new RomanNumeral('MMMCMXCIX');
+            sut.toString().should.equal('MMMCMXCIX');
+        });
+
         it('should be displayed correctly with toString', () => {
             var sut = new RomanNumeral('MMXVII');
+            sut.toString().should.equal('MMXVII');
+        });
+    });
+
+    describe('when displaying an integer', () => {
+        it('should convert MMXVII to 2017', () => {
+            var sut = new RomanNumeral('MMXVII');
+            sut.toInteger().should.equal(2017);
+        });
+    });
+
+    describe('when parsing an integer', () => {
+        it('should raise an exception when less than 1', () => {
+            RomanNumeral.parseInteger(0)
+                .should
+                .throw(RangeError, 'between I (1) and MMMCMXCIX (3999)');
+        });
+
+        it ('should raise an exception when greater than 3999', () => {
+            RomanNumeral.parseInteger(4000)
+                .should
+                .throw(RangeError, 'between I (1) and MMMCMXCIX (3999)');
+        });
+
+        it('should convert 1 to I', () => {
+            var sut = RomanNumeral.parseInteger(1);
+            sut.toString().should.equal('I');
+        });
+
+        it('should convert 3999 to MMMCMXCIX', () => {
+            var sut = RomanNumeral.parseInteger(3999);
+            sut.toString().should.equal('MMMCMXCIX');
+        });
+
+        it('should convert 2017 to MMXVII', () => {
+            var sut = RomanNumeral.parseInteger(2017);
             sut.toString().should.equal('MMXVII');
         });
     });
