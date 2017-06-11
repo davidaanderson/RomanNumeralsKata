d494aefee09e1e78d6d405375129a7511541fb9a
David Anderson <david@anderson.scot>
2017-06-11 11:51:29 +0100

    Initialised module with stub test, test runner and linter

---
+++ tests/RomanNumeral-spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('the RomanNumeral class', function () {
+    describe('when constructed with a roman numeral', function() {
+        it('should be displayed with toString', function () {
+            expect(true).to.be.false;
+        });   
+    });
+});
